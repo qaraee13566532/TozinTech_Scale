3ae1aca8bafecc0745e1e3db6d8007bc7aa00dd9
qaraee13566532 <davoud.qaraee@gmail.com>
2023-10-28 00:53:24 +0330

    modification start

---
+++ main/core/data_types/plu/plu.cpp
@@ -0,0 +1,29 @@
+#include "constant.h"
+
+class Plu
+{
+    unsigned long   sellPrice;
+    unsigned long   buyPrice;
+    unsigned long   pluNumber;  
+    unsigned int    pluName[MAX_PLU_NAME_LENGHT];
+    unsigned char   barcodeID;
+    unsigned char   pluType;
+    unsigned char   pluUnit;
+    unsigned char   discountID;
+    unsigned char   taxID;
+	bool            isDeleted;
+	bool            isActived;
+	unsigned  char  groupID;
+	unsigned  char  department;
+	unsigned  int   expireDate;
+	unsigned  int   expireTime;
+	unsigned  char  barcode[MAX_BARCODE_LENGHT];	
+    public : void setData(void)
+    {
+        barcodeID=10;
+    }
+    public : unsigned char getData(void)
+    {
+        return barcodeID;
+    }
+};
--- main/main.cpp
+++ main/main.cpp
@@ -2,6 +2,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "systeminfo.h"
+#include "plu.cpp"
 
 extern "C"
 {
@@ -10,12 +11,14 @@ extern "C"
 
 void app_main(void)
 {
+    Plu pluData;
+    pluData.setData();
     printf("Hello world!\n");
     esp_systeminfo info;
     info.print();
     for (int i = 10; i >= 0; i--)
     {
-        printf("Restarting in %d seconds...\n", i);
+        printf("Restarting in %d seconds... %d\n", i,pluData.getData());
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
     printf("Restarting now.\n");
